196ea73511c53d340ad73e785793df8bd6927e13
YujinBae <43170445+YujinBae@users.noreply.github.com>
2018-11-15 19:53:52 +0900

    Update view.py

--- supplementary_lessons/A_Group/yujin/app_ranking/11.13/view.py
+++ supplementary_lessons/A_Group/yujin/app_ranking/11.13/view.py
@@ -1,4 +1,3 @@
-# from app3 import *
 import app3
 import main3 
 
@@ -26,7 +25,6 @@ class View:
             else:quit()
 
 
-
     # 불러올 데이터 타입 입력
     def choiceOutput():
         rankType=input(
@@ -41,10 +39,10 @@ class View:
         ''')            
         if rankType=='q':
             quit() 
-        else:
+        elif rankType in ('2','5'):
             visualType=input(
-                # 일단 날짜는 제일 최신것으로 고정
-                # 후에 일자 지정 및 기간 지정 가능하게 고치기
+            # 일단 날짜는 제일 최신것으로 고정
+            # 후에 일자 지정 및 기간 지정 가능하게 고치기
             '''
                 원하는 데이터 시각화 서비스를 선택해주세요.
                 1 : 10순위 내 앱 기본 정보
@@ -60,32 +58,54 @@ class View:
                 main3.getData.genreProp(rankType)
             elif visualType =='3':
                 main3.getData.priceHist(rankType)
-
-
-
+            elif visualType =='4':
+                main3.getData.scoreDesc(rankType)
+            elif visualType =='q':
+                quit()
+        else:
+            visualType=input(
+            # 일단 날짜는 제일 최신것으로 고정
+            # 후에 일자 지정 및 기간 지정 가능하게 고치기
+            '''
+                원하는 데이터 시각화 서비스를 선택해주세요.
+                1 : 10순위 내 앱 기본 정보
+                2 : 50순위 내 앱 장르별 비율
+                3 : 앱 평가점수 기준 10순위 내림차순 도표
+                q : 프로그램 종료
+            ''')  
+            if visualType =='1':              
+                # 후에 창으로 띄우는 거로 수정해보기     
+                main3.getData.initDB(rankType)
+            elif visualType =='2':
+                main3.getData.genreProp(rankType)
+            elif visualType =='3':
+                main3.getData.scoreDesc(rankType)
+            elif visualType =='q':
+                quit()
 
 
     # 받아온 데이터 보여주고 확인하기
     def check(doc_df2):
         print(doc_df2.head())
-        checking=input('''
-        원하는 랭킹 데이터가 맞습니까?
-        예 : y 아니오 : n
-        ('예' 선택 시 DB에 적재)
+        checking=input(
+        '''
+            원하는 랭킹 데이터가 맞습니까?
+            예 : y 아니오 : n
+            ('예' 선택 시 DB에 적재)
         ''')
         if checking =='y':
             app3.Model.genrestruc(doc_df2)
         else:
             rankType=input(
             '''
-            적재할 랭킹 데이터의 어플 종류를 입력해주세요.
-            (데이터 로딩까지 약 4분정도 소요됩니다)
-            1 : 무료
-            2 : 유료
-            3 : 매출
-            4 : 신규무료
-            5 : 신규유료
-            q : 프로그램 종료
+                적재할 랭킹 데이터의 어플 종류를 입력해주세요.
+                (데이터 로딩까지 약 4분정도 소요됩니다)
+                1 : 무료
+                2 : 유료
+                3 : 매출
+                4 : 신규무료
+                5 : 신규유료
+                q : 프로그램 종료
             ''')            
             if rankType=='q':
                 quit()
