a395ac4c8a9658c444979f47691aa274a09d2e6b
YujinBae <43170445+YujinBae@users.noreply.github.com>
2018-11-15 19:56:59 +0900

    Update main3.py

--- supplementary_lessons/A_Group/yujin/app_ranking/11.13/main3.py
+++ supplementary_lessons/A_Group/yujin/app_ranking/11.13/main3.py
@@ -3,29 +3,34 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import re
 import seaborn as sns
+from datetime import datetime
+import os
 
 class getData:
-    ## 적재된거 가져오기
-    # 디비 접속
-    def initDB(rankType):
+    # 10순위 내 앱 기본 정보
+    def initDB(rankType,batch='console'):
         conn = sql.connect(
-                        host='127.0.0.1',            
-                        user='root',
-                        password='12341234',
-                        db='pythondb',
-                        charset='utf8',
-                        cursorclass=sql.cursors.DictCursor
-                        )
+                    host='127.0.0.1',            
+                    user='root',
+                    password='12341234',
+                    db='pythondb',
+                    charset='utf8',
+                    cursorclass=sql.cursors.DictCursor
+                    )
         tbl_name = 'app_ranking_%s' % rankType
-        df = pd.read_sql('select * from ' + tbl_name , con = conn)
-        print(df.head(10))
+        subquery = '(SELECT * FROM %s ORDER BY date DESC LIMIT 50) lastdata' % tbl_name
+        df = pd.read_sql('select * from %s' % subquery , con = conn)
+        pd.set_option('display.max_columns', 20)
+        finaldf=df.head(10)
+        if batch=='console':
+            print(finaldf)
+
+        getData.saveCsv(finaldf,rankType,'basic',batch)
+
         conn.close()
-
-        # df = pd.read_sql('select Date, Close from ' + tableName + ' where Date >= 20151101' , con = conn)
-
-
+        
     #  50순위 내 앱 장르별 비율 
-    def genreProp(rankType):
+    def genreProp(rankType,batch='console'):
         conn = sql.connect(
                         host='127.0.0.1',            
                         user='root',
@@ -35,8 +40,8 @@ class getData:
                         cursorclass=sql.cursors.DictCursor
                         )
         tbl_name = 'app_ranking_%s' % rankType
-
-        df = pd.read_sql('select * from ' + tbl_name , con = conn)
+        subquery = '(SELECT * FROM %s ORDER BY date DESC LIMIT 50) lastdata' % tbl_name
+        df = pd.read_sql('select * from %s' % subquery , con = conn)
 
         genrelist = list()
         for genre in df['genre']:
@@ -53,32 +58,37 @@ class getData:
         for genre in genrelist:
             genredict[genre] += 1
 
-        # print(genreSer)
-
         # 2% 이하일 경우 합하여 기타로 빼기
-        # genredict.values()
         countgenre=len(genrelist)
         standard = len(genrelist) * 0.02
         genredict_cp=genredict.copy()
+        genredict["'etc'"]=0
         for genre in genredict_cp:
             if genredict[genre] < standard:
+                genredict["'etc'"] += genredict[genre]
                 genredict.pop(genre)
-
+    
         genreSer = pd.Series(genredict)
 
         # 파이차트 그리기
-        # 라벨과 퍼센트 붙이기 ========================================
-        plt.pie(genreSer,labels=genreSer.index,autopct='%1.1f%%')
-        
-        plt.show()
+        # 비율이 제일 큰게 떨어져 나오지며, 비율이 큰 순대로 이어지게 만들기
+        explode = [0 for i in range(len(genreSer))]
+        explode[0]=0.1
+        explode=tuple(explode)
+        fig=plt.figure(figsize=(9,9))
+        sns.set_palette("coolwarm", 7)
+        genreSer = genreSer.sort_values(ascending=False)
+        plt.pie(genreSer,explode=explode,labels=genreSer.index,autopct='%1.1f%%')
+        plt.title('Proportion of Genres')
+        if batch=='console':
+            plt.show()
 
-
-        # print(df.head(10))
+        getData.saveImg(fig,rankType,'genrepie',batch)
         conn.close()        
 
 
     # 가격을 범위를 구분하여 범위별 빈도 수
-    def priceHist(rankType):
+    def priceHist(rankType,batch='console'):
         conn = sql.connect(
                         host='127.0.0.1',            
                         user='root',
@@ -88,14 +98,10 @@ class getData:
                         cursorclass=sql.cursors.DictCursor
                         )
         tbl_name = 'app_ranking_%s' % rankType
+        subquery = '(SELECT * FROM %s ORDER BY date DESC LIMIT 50) lastdata' % tbl_name
+        df = pd.read_sql('select price from %s' % subquery , con = conn)
 
-        df = pd.read_sql('select price from ' + tbl_name , con = conn)
-
-        # 먼저 리스트로 받고 -> for 문
-        # 하나씩 나눠 받아서 ,을 없애고 -> 정규식
-        # 최소값 최대값 구한 뒤 -> min, max
-        # 계급 설정
-
+        # ,을 없애고 최소값 최대값 구한 뒤 계급 설정
         p=re.compile('[\,]')
         price1=[]
         for price in df['price']:
@@ -104,87 +110,51 @@ class getData:
             else:
                 price1.append(p.sub('',price))
         price1 = list(map(int, price1))
+        
         # 1000 ~ 33000 가격 존재
-        # print(min(price1),max(price1))
-
         # 98개 나옴 (2개 None값)
-        # print(len(price1))
-
         # 3000원 단위로 계급 10개 나누기
-        # pricedict={'less3000':0,'3000to5999':0,'6000to8999':0,'9000to11999':0,'12000to14999':0,
-        # '15000to17999':0,'18000to20999':0,'21000to23999':0,'24000to26999':0,'more30000':0}
-        # for price in price1:
-        #     if price<3000:
-        #         pricedict['less3000'] += 1
-        #     elif price<6000:
-        #         pricedict['3000to5999'] += 1
-        #     elif price<9000:
-        #         pricedict['6000to8999'] += 1
-        #     elif price<12000:
-        #         pricedict['9000to11999'] += 1    
-        #     elif price<15000:
-        #         pricedict['12000to14999'] += 1
-        #     elif price<18000:
-        #         pricedict['15000to17999'] += 1
-        #     elif price<21000:
-        #         pricedict['18000to20999'] += 1
-        #     elif price<24000:
-        #         pricedict['21000to23999'] += 1
-        #     elif price<27000:
-        #         pricedict['24000to26999'] += 1    
-        #     else:
-        #         pricedict['more30000'] += 1
-        pricelist=[]
+        pricedict={'less3000':0,'3000to5999':0,'6000to8999':0,'9000to11999':0,'12000to14999':0,
+        '15000to17999':0,'18000to20999':0,'21000to23999':0,'24000to26999':0,'more30000':0}
         for price in price1:
             if price<3000:
-                price='less3000'
-                pricelist.append(price)
+                pricedict['less3000'] += 1
             elif price<6000:
-                price='3000to5999'
-                pricelist.append(price)
+                pricedict['3000to5999'] += 1
             elif price<9000:
-                price='6000to8999'
-                pricelist.append(price)
+                pricedict['6000to8999'] += 1
             elif price<12000:
-                price='9000to11999'    
-                pricelist.append(price)
+                pricedict['9000to11999'] += 1    
             elif price<15000:
-                price='12000to14999'
-                pricelist.append(price)
+                pricedict['12000to14999'] += 1
             elif price<18000:
-                price='15000to17999'
-                pricelist.append(price)
+                pricedict['15000to17999'] += 1
             elif price<21000:
-                price='18000to20999'
-                pricelist.append(price)
+                pricedict['18000to20999'] += 1
             elif price<24000:
-                price='21000to23999'
-                pricelist.append(price)
+                pricedict['21000to23999'] += 1
             elif price<27000:
-                price='24000to26999'
-                pricelist.append(price)
+                pricedict['24000to26999'] += 1    
             else:
-                price='more30000'
-                pricelist.append(price)
-        # print(pricelist)
-        # priceSer = pd.Series(pricedict)
-        # plt.hist(priceSer)
-        # plt.show()
+                pricedict['more30000'] += 1
 
-        # sns.countplot(priceSer)
-        sns.countplot(pricelist)
-        plt.title('Histogram of Price')
-        # 가격 범위 오름차순 혹은 빈도수 내림차순으로 재정렬
-        # 가능하면 빈도수 0인 항목도 표시되게
-        plt.show()
+        priceSer = pd.Series(pricedict)
 
-        # print(df)
+        fig=plt.figure(figsize=(14,6))
+        ax=sns.barplot(priceSer.index, priceSer.values,palette='RdBu') 
+        ax.set(xlabel="Price class", ylabel='Frequency')
+        ax.set_title('Frequencies of Price class', y=1.02)
+        # 가능하면 후에 비율 %로 annot
+        for p in ax.patches:
+            ax.annotate('{}'.format(int(p.get_height())), (p.get_x()+0.35, p.get_height()+0.4))
+        if batch=='console':
+            plt.show()
+        getData.saveImg(fig,rankType,'pricebar',batch)
         conn.close() 
 
 
-
     # 앱 평가점수 기준 10순위 내림차순 도표
-    def scoreDesc(rankType):
+    def scoreDesc(rankType,batch='console'):
         conn = sql.connect(
                         host='127.0.0.1',            
                         user='root',
@@ -199,21 +169,64 @@ class getData:
 
         pd.set_option('display.max_columns', 20)
         # 나중에 gameName 왼쪽 혹은 오른쪽 정렬 해놓기
+        finaldf=df.head(10)
+        if batch=='console':
+            print(finaldf)
+        getData.saveCsv(finaldf,rankType,'scoredesc',batch)
+        conn.close() 
 
-        print(df[:10])
+    # 저장여부 물어보고 저장하는 함수
+    def saveImg(fig,rankType,plotType,batch):
+        if batch =='batch':
+            save_q = 'y'
+        else:        
+            save_q=input(
+            '''
+                이미지를 저장하시겠습니까?
+                예 : y 아니오 : n
+                ('예' 선택 시 폴더에 적재, '아니오' 선택 시 프로그램 종료)
+            '''
+            )
+        if save_q=='y':
+            if batch=='console':
+                path = './console_image' 
+            else:
+                path = './batch_image' 
+            if not os.path.exists(path): 
+                os.mkdir(path)    
+            path = path + '/ranktype' + rankType 
+            if not os.path.exists(path): 
+                os.mkdir(path)                           
+            nowtime = datetime.now().strftime('%Y%m%d%H%M%S')
+            fig.savefig(path + '/%s_%s_%s.png' % (plotType,nowtime,rankType))
+        else:
+            quit()
 
 
+    def saveCsv(finaldf,rankType,plotType,batch):
+        if batch =='batch':
+            save_q = 'y'
+        else:
+            save_q=input(
+            '''
+                데이터프레임을 저장하시겠습니까?
+                예 : y 아니오 : n
+                ('예' 선택 시 폴더에 적재, '아니오' 선택 시 프로그램 종료)
+            '''
+            )   
+        if save_q=='y':
+            if batch=='console':
+                path = './console_csv'
+            else:
+                path = './batch_csv'        
+            if not os.path.exists(path): 
+                os.mkdir(path)     
+            path = path + '/ranktype' + rankType 
+            if not os.path.exists(path): 
+                os.mkdir(path)                      
+            nowtime = datetime.now().strftime('%Y%m%d%H%M%S')
+            finaldf.to_csv(path + '/%s_%s_%s.csv' % (plotType,nowtime,rankType), encoding='utf-8-sig')
+        else:
+            quit()
 
-getData.genreProp('5')
 
-# getData.priceHist('5')
-
-# getData.scoreDesc('5')
-
-
-
-# a=getData.priceHist('5')
-# print(a.iloc[:6].values)
-# if a.iloc[4].any():
-#     print('None아님')
-# else:print('none')
