0f7733f465372d5eadd111cc24b581eaf2ec9ef1
YujinBae <43170445+YujinBae@users.noreply.github.com>
2018-11-30 15:56:24 +0900

    Update __init__.py

--- supplementary_lessons/A_Group/yujin/app_ranking/11.16/model/__init__.py
+++ supplementary_lessons/A_Group/yujin/app_ranking/11.16/model/__init__.py
@@ -1,15 +1,15 @@
 import pymysql as my
-
+import os
 
 def selectData(rankType):
     rows = None # 쿼리 결과        
-    conn = sql.connect(
+    conn = my.connect(
             host='127.0.0.1',            
             user='root',
             password='12341234',
             db='pythondb',
             charset='utf8',
-            cursorclass=sql.cursors.DictCursor
+            cursorclass=my.cursors.DictCursor
             )
 
     tbl_name = 'app_ranking_%s' % rankType
@@ -17,14 +17,52 @@ def selectData(rankType):
 
     try:            
         with conn.cursor() as cursor:
-            sql    = 'select * from %s'           
-            cursor.execute( sql, (subquery, ))
+            sql    = 'select * from %s' % subquery
+            cursor.execute( sql )
             rows   = cursor.fetchall()
     except Exception as e:
         rows = None
-        # print('에러->',e)        
+        print('에러->',e)        
     return rows
     # conn.close()
+
+
+def getFiles(rankType):
+    path_dir = 'C:/Users/idec/Documents/analysis_project/App_ranking/static/batch_image/rankType%s' % rankType
+    # path_dir3 = 'C:/Users/idec/Documents/analysis_project/App_ranking/console_csv'
+    # path_dir4 = 'C:/Users/idec/Documents/analysis_project/App_ranking/console_image'
+    # path_dir = [path_dir1, path_dir2]
+    file_list = os.listdir(path_dir)
+    # print(file_list)
+    return file_list
+
+
+
+
+def selectDesc(rankType):
+    rows = None # 쿼리 결과        
+    conn = my.connect(
+            host='127.0.0.1',            
+            user='root',
+            password='12341234',
+            db='pythondb',
+            charset='utf8',
+            cursorclass=my.cursors.DictCursor
+            )
+
+    tbl_name = 'app_ranking_%s' % rankType
+    subquery = '(SELECT * FROM %s ORDER BY date DESC LIMIT 50) lastdata' % tbl_name
+
+    try:            
+        with conn.cursor() as cursor:
+            sql    = 'select ranking, gameName, score, developer, genre from %s order by score desc' % subquery
+            cursor.execute( sql )
+            rows   = cursor.fetchall()
+    except Exception as e:
+        rows = None
+        print('에러->',e)        
+    return rows
+
 
 
 
