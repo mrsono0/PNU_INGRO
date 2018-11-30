4262f9fde37a6720658c50728afe522d1ce0507d
YujinBae <43170445+YujinBae@users.noreply.github.com>
2018-11-30 15:53:02 +0900

    Update flask_controller.py

--- supplementary_lessons/A_Group/yujin/app_ranking/11.16/flask_controller.py
+++ supplementary_lessons/A_Group/yujin/app_ranking/11.16/flask_controller.py
@@ -1,6 +1,7 @@
 from flask import Flask, url_for, render_template, request, redirect
-from model import selectData, getFiles
+from model import selectData, getFiles, selectDesc
 from model_read import getData
+import main_controller
 app = Flask(__name__)
 
 rankType=None
@@ -20,10 +21,8 @@ def home():
                 return render_template('step2_read_134.html',rankType=rankType)
         else:
             rankType = request.form['rankType_w']
-            if rankType in ('2','5'):
-                return render_template('step2_write_25.html',rankType=rankType)
-            else:
-                return render_template('step2_write_134.html',rankType=rankType)
+            main_controller.Start.flask_write(rankType)
+            return render_template('step2_write_25.html',rankType=rankType)
             # if id(option1/option2)가 체크되어 있으면=====================
             # 시각화 종류 선택 페이지 만들기 ===========================
             # 받아온 데이터 보여주고 확인하는 페이지 만들기 =============
@@ -40,7 +39,26 @@ def visual():
             return render_template('step3_read_1_134.html',data=rows)
     elif visualType =='2':
         rows = getFiles(rankType)
-        return render_template('step3_read_2.html', data=rows )
+        genrerow =[]
+        for row in rows:
+            if row.startswith("genrepie"):
+                genrerow.append(row)
+        return render_template('step3_read_2.html', data=genrerow )
+
+    elif visualType == '3':
+        rows = selectDesc(rankType)
+        rows = rows[:10]
+        return render_template('step3_read_3.html',data=rows)
+
+    elif visualType =='4':
+        rows = getFiles(rankType)
+        genrerow =[]
+        for row in rows:
+            if row.startswith("pricebar"):
+                genrerow.append(row)
+        return render_template('step3_read_2.html', data=genrerow )
+
+
 
 @app.route('/image', methods=['GET'])
 def image():
@@ -48,26 +66,10 @@ def image():
     return render_template('image.html',rankType=rankType,filename=filename)
 
 
-
-'''
-if uid and upw: # 정상
-    #3. 데이터가 정상이면 디비 조회
-    row = loginSql(uid,upw)
-    if row:
-        #4. 회원이 맞으면 ok -> 세션생성(후처리) -> 서비스 이동
-        # 요청을 다른 url로 돌려준다
-        return redirect(url_for('home'))
-    else:
-    #4. 회원이 아니면 fail -> 경고 처리 후 돌려보낸다
-        return render_template('err.html', msg='회원이 아니시잖아요!!')
-    #return '정상응답'
-else:#비정상처리
-    #2. 데이터가 없는 경우? -> 경고 처리 돌려보낸다
-    return render_template('err.html', msg='입력이 정확하지 않습니다.')
-'''
-
-
-
 if __name__ =='__main__':
     app.run(debug=True)
+
+
+
+
 
